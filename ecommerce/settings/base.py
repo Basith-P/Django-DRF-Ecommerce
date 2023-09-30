6c86679b7d7402045f337c72768ebc3e6a8626e2
Basith P <basithpofficial@gmail.com>
2023-09-30 17:59:02 +0530

    feat: add pytest

--- ecommerce/settings/base.py
+++ ecommerce/settings/base.py
@@ -19,6 +19,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # External
+    "rest_framework",
+    # Internal
 ]
 
 MIDDLEWARE = [
@@ -92,3 +95,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {}
