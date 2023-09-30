290bce0867c26ed93eaa96c3e1320439cac9a36c
Basith P <basithpofficial@gmail.com>
2023-09-30 20:59:25 +0530

    feat: create models in product app

--- apps/product/models.py
+++ apps/product/models.py
@@ -1,14 +1,27 @@
 from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
 
 
-class Category(models.Model):
+class Category(MPTTModel):
     name = models.CharField(max_length=255)
     # slug = models.SlugField(max_length=255, unique=True)
+    parent = TreeForeignKey(
+        "self", on_delete=models.PROTECT, null=True, blank=True, related_name="children"
+    )
+
+    class MPTTMeta:
+        order_insertion_by = ["name"]
+
+    def __str__(self) -> str:
+        return self.name
 
 
 class Brand(models.Model):
     name = models.CharField(max_length=255)
     # slug = models.SlugField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Product(models.Model):
@@ -18,3 +31,9 @@ class Product(models.Model):
     is_digital = models.BooleanField(default=False)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    category = TreeForeignKey(
+        Category, null=True, blank=True, on_delete=models.SET_NULL
+    )
+
+    def __str__(self):
+        return self.name
