91f56b2f4a19ea9e4cdefa8302bd9c799b7a476d
Basith P <basithpofficial@gmail.com>
2023-09-30 22:30:16 +0530

    feat: add product and brand api routes

--- apps/product/views.py
+++ apps/product/views.py
@@ -11,3 +11,21 @@ class CategoryViewSet(viewsets.ModelViewSet):
 
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+
+class BrandViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing brand instances.
+    """
+
+    queryset = Brand.objects.all()
+    serializer_class = BrandSerializer
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing product instances.
+    """
+
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,26 +1,19 @@
 from django.contrib import admin
 from django.urls import include, path
-from drf_spectacular.views import (
-    SpectacularAPIView,
-    SpectacularRedocView,
-    SpectacularSwaggerView,
-)
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView
 from rest_framework.routers import DefaultRouter
 
-from apps.product.views import CategoryViewSet
+from apps.product.views import BrandViewSet, CategoryViewSet, ProductViewSet
 
 router = DefaultRouter()
+router.register("brand", BrandViewSet)
 router.register("category", CategoryViewSet)
+router.register("product", ProductViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/", include(router.urls)),
     path("api/v1/schema/", SpectacularAPIView.as_view(), name="schema"),
-    # path(
-    #     "api/v1/schema/docs/",
-    #     SpectacularSwaggerView.as_view(url_name="schema"),
-    #     name="docs",
-    # ),
     path(
         "api/v1/schema/docs/",
         SpectacularRedocView.as_view(url_name="schema"),
